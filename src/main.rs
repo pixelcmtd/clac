f4209e6c8012800623d194fcc93f4085c7bbe81a
pixel <chrissx@chrissx.de>
2021-12-11 15:41:59 +0100

    support for maths syntax and many more changes

--- src/main.rs
+++ src/main.rs
@@ -13,23 +13,24 @@ struct ΛParser;
 
 #[derive(Clone, Debug, PartialEq)]
 enum ΛNode {
-    Symbol(String),
-    Lambda(String, Box<ΛNode>),
-    Application(Box<ΛNode>, Box<ΛNode>),
-    Assignment(String, Box<ΛNode>),
-    Definition(String, Box<ΛNode>),
+    Σ(String),
+    Λ(String, Box<ΛNode>), //ΤNode
+    Α(Box<ΛNode>, Box<ΛNode>),
+    Χ(String, Box<ΛNode>),
+    //Τ(String, Box<ΛType>),
 }
 
-// TODO:
-//enum ΛDialect {
-//    CompSci,
-//    Maths,
-//    ΜHaskell,
+// TODO: think about an any type
+//enum ΤNode {
+//    Π(String),
+//    Σ(String),
+//    Λ(Box<ΤNode>, Box<ΤNode>),
+//    Υ(Box<ΤNode>, Box<ΤNode>),
 //}
 
 // TODO:
 //impl PartialEq for ΛNode {
-//    // TODO: η reduction and α conversion
+//    // TODO: α-conversion (or η-reduction, maybe β-reduction)
 //    fn eq(&self, other: &Self) -> bool {
 //        match self {
 //            ΛNode::Symbol(s) => match other {
@@ -49,58 +50,62 @@ enum ΛNode {
 //}
 
 impl ΛNode {
+    fn λ(params: Vec<String>, body: ΛNode) -> ΛNode {
+        let mut params = params.into_iter().rev();
+        let mut func = ΛNode::Λ(params.next().unwrap(), Box::from(body));
+        for param in params {
+            func = ΛNode::Λ(String::from(param.as_str()), Box::from(func));
+        }
+        func
+    }
+
+    fn α(func: ΛNode, arg: ΛNode) -> ΛNode {
+        ΛNode::Α(Box::from(func), Box::from(arg))
+    }
+
+    fn χ(name: String, body: ΛNode) -> ΛNode {
+        ΛNode::Χ(name, Box::from(body))
+    }
+
     fn from_parse_tree(tree: Pair<Rule>) -> Option<Self> {
         match tree.as_rule() {
-            Rule::WHITESPACE | Rule::params | Rule::statement => None,
-            Rule::variable => Some(ΛNode::Symbol(String::from(tree.as_str()))),
-            Rule::item | Rule::body | Rule::pexpr => {
+            Rule::WHITESPACE | Rule::params | Rule::mparams | Rule::statement => None,
+            Rule::variable | Rule::mvariable => Some(ΛNode::Σ(String::from(tree.as_str()))),
+            Rule::item | Rule::mitem | Rule::body | Rule::mbody => {
                 ΛNode::from_parse_tree(tree.into_inner().next()?)
             }
-            Rule::expr => {
+            Rule::expr | Rule::mexpr => {
                 let mut items: Vec<Pair<Rule>> = tree.into_inner().rev().collect();
                 Some(if items.len() == 1 {
                     ΛNode::from_parse_tree(items.pop()?)?
                 } else {
-                    let mut expr = ΛNode::Application(
-                        Box::from(ΛNode::from_parse_tree(items.pop()?)?),
-                        Box::from(ΛNode::from_parse_tree(items.pop()?)?),
+                    let mut expr = ΛNode::α(
+                        ΛNode::from_parse_tree(items.pop()?)?,
+                        ΛNode::from_parse_tree(items.pop()?)?,
                     );
                     for item in items {
-                        expr = ΛNode::Application(
-                            Box::from(expr),
-                            Box::from(ΛNode::from_parse_tree(item)?),
-                        );
+                        expr = ΛNode::α(expr, ΛNode::from_parse_tree(item)?);
                     }
                     expr
                 })
             }
-            Rule::func => {
+            Rule::func | Rule::mfunc => {
                 let mut inner = tree.into_inner().filter(|x| match x.as_rule() {
-                    Rule::params | Rule::body => true,
+                    Rule::params | Rule::mparams | Rule::body | Rule::mbody => true,
                     _ => false,
                 });
-                let mut params = inner.next()?.into_inner().rev();
+                let params = inner.next()?.into_inner();
                 let body = inner.next()?;
-                let mut func = ΛNode::Lambda(
-                    String::from(params.next()?.as_str()),
-                    Box::from(ΛNode::from_parse_tree(body)?),
-                );
-                for param in params {
-                    func = ΛNode::Lambda(String::from(param.as_str()), Box::from(func));
-                }
-                Some(func)
+                Some(ΛNode::λ(
+                    params.map(|x| String::from(x.as_str())).collect(),
+                    ΛNode::from_parse_tree(body)?,
+                ))
             }
-            Rule::definition => {
+            Rule::vardef => {
                 let mut inner = tree.into_inner();
                 let name = inner.next()?.as_str();
                 let body = ΛNode::from_parse_tree(inner.next()?)?;
-                Some(ΛNode::Definition(String::from(name), Box::from(body)))
-            }
-            Rule::assignment => {
-                let mut inner = tree.into_inner();
-                let name = inner.next()?.as_str();
-                let body = ΛNode::from_parse_tree(inner.next()?)?;
-                Some(ΛNode::Assignment(String::from(name), Box::from(body)))
+                Some(ΛNode::χ(String::from(name), body))
             }
         }
     }
@@ -118,108 +123,82 @@ impl ΛNode {
 
     fn to_string(&self) -> String {
         match self {
-            ΛNode::Symbol(s) => s.clone(),
-            ΛNode::Definition(name, expr) => name.clone() + " ← " + &expr.to_string(),
-            ΛNode::Assignment(name, expr) => name.clone() + " ≔ " + &expr.to_string(),
-            ΛNode::Lambda(arg, body) => String::from("λ") + arg + "." + &body.to_string(),
-            ΛNode::Application(func, arg) => {
+            ΛNode::Σ(s) => s.clone(),
+            ΛNode::Χ(name, expr) => name.clone() + " ← " + &expr.to_string(),
+            ΛNode::Λ(arg, body) => String::from("λ") + arg + "." + &body.to_string(),
+            ΛNode::Α(func, arg) => {
                 (match **func {
-                    ΛNode::Symbol(_) | ΛNode::Application(_, _) => func.to_string(),
-                    ΛNode::Lambda(_, _) | ΛNode::Definition(_, _) | ΛNode::Assignment(_, _) => {
+                    ΛNode::Σ(_) | ΛNode::Α(_, _) => func.to_string(),
+                    ΛNode::Λ(_, _) | ΛNode::Χ(_, _) => {
                         String::from("(") + &func.to_string() + ")"
                     }
                 } + &(match **arg {
-                    ΛNode::Symbol(_) => {
+                    ΛNode::Σ(_) => {
                         String::from(match **func {
-                            ΛNode::Symbol(_) => " ",
+                            ΛNode::Σ(_) => " ",
                             _ => "",
                         }) + &arg.to_string()
                     }
-                    ΛNode::Lambda(_, _)
-                    | ΛNode::Application(_, _)
-                    | ΛNode::Definition(_, _)
-                    | ΛNode::Assignment(_, _) => String::from("(") + &arg.to_string() + ")",
+                    ΛNode::Λ(_, _) | ΛNode::Α(_, _) | ΛNode::Χ(_, _) => {
+                        String::from("(") + &arg.to_string() + ")"
+                    }
                 }))
             }
         }
     }
 
-    fn η_reduce(&self) -> ΛNode {
-        match self {
-            ΛNode::Symbol(_) => self.clone(),
-            // TODO: rethink whether this is actually a good idea
-            ΛNode::Definition(n, e) => ΛNode::Definition(n.clone(), Box::from(e.η_reduce())),
-            ΛNode::Assignment(n, e) => ΛNode::Assignment(n.clone(), Box::from(e.η_reduce())),
-            ΛNode::Lambda(a, b) => ΛNode::Lambda(
-                a.clone(),
-                Box::from(
-                    (match &**b {
-                        ΛNode::Application(f, p) => {
-                            if match &**p {
-                                ΛNode::Symbol(s) => *a == *s,
-                                _ => false,
-                            } {
-                                &f
-                            } else {
-                                b
-                            }
-                        }
-                        _ => b,
-                    })
-                    .η_reduce(),
-                ),
-            ),
-            ΛNode::Application(f, a) => {
-                ΛNode::Application(Box::from(f.η_reduce()), Box::from(a.η_reduce()))
-            }
-        }
-    }
-
-    fn β_reduce(&self) -> ΛNode {
-        β_reduce(self, &String::new(), &ΛNode::Symbol(String::new()))
+    fn reduce(&self) -> ΛNode {
+        reduce(self, &String::new(), &ΛNode::Σ(String::new()))
     }
 }
 
-fn β_reduce(node: &ΛNode, name: &String, arg: &ΛNode) -> ΛNode {
+fn reduce(node: &ΛNode, name: &String, arg: &ΛNode) -> ΛNode {
     match node {
-        ΛNode::Symbol(ref s) => if *s == *name { arg } else { node }.clone(),
+        ΛNode::Σ(ref s) => if *s == *name { arg } else { node }.clone(),
         // TODO: rethink whether this is actually a good idea
-        ΛNode::Definition(n, e) => {
-            ΛNode::Definition(n.clone(), Box::from(β_reduce(e, name, arg)))
-        }
-        ΛNode::Assignment(n, e) => {
-            ΛNode::Assignment(n.clone(), Box::from(β_reduce(e, name, arg)))
-        }
-        ΛNode::Lambda(param, body) => {
-            if *param == *name {
-                node.clone()
-            } else {
-                ΛNode::Lambda(param.clone(), Box::from(β_reduce(body, name, arg)))
-            }
-        }
-        ΛNode::Application(func, param) => {
-            let func = β_reduce(func, name, arg);
-            let param = β_reduce(param, name, arg);
-            match func {
-                ΛNode::Lambda(fparam, body) => {
-                    β_reduce(&β_reduce(&*body, &fparam, &param), name, arg)
+        ΛNode::Χ(n, e) => ΛNode::Χ(n.clone(), Box::from(reduce(e, name, arg))),
+        ΛNode::Λ(param, body) => match &**body {
+            ΛNode::Α(func, prm) => {
+                if match &**prm {
+                    ΛNode::Σ(s) => *param == *s,
+                    _ => false,
+                } {
+                    reduce(&*func, name, arg)
+                } else if *param == *name {
+                    node.clone()
+                } else {
+                    ΛNode::Λ(param.clone(), Box::from(reduce(body, name, arg)))
                 }
-                _ => ΛNode::Application(Box::from(func), Box::from(param)),
+            }
+            _ => {
+                if *param == *name {
+                    node.clone()
+                } else {
+                    ΛNode::Λ(param.clone(), Box::from(reduce(body, name, arg)))
+                }
+            }
+        },
+        ΛNode::Α(func, param) => {
+            let func = reduce(func, name, arg);
+            let param = reduce(param, name, arg);
+            match func {
+                ΛNode::Λ(fparam, body) => reduce(&reduce(&*body, &fparam, &param), name, arg),
+                _ => ΛNode::Α(Box::from(func), Box::from(param)),
             }
         }
     }
 }
 
 struct ΛCalculus {
-    definitions: HashMap<String, ΛNode>,
-    assignments: HashMap<String, ΛNode>,
+    vardefs: HashMap<String, ΛNode>,
+    //typedefs: HashMap<String, ΤNode>,
 }
 
 impl ΛCalculus {
     fn new() -> Self {
         ΛCalculus {
-            definitions: HashMap::new(),
-            assignments: HashMap::new(),
+            vardefs: HashMap::new(),
+            //typedefs: HashMap::new(),
         }
     }
 
@@ -230,6 +209,19 @@ impl ΛCalculus {
             .filter_map(Some)
             .map(|x| x.unwrap())
             .collect()
+    }
+
+    fn eval(&mut self, tree: ΛNode) -> ΛNode {
+        let tree = tree.reduce();
+        match tree.clone() {
+            // TODO:
+            ΛNode::Σ(_) | ΛNode::Λ(_, _) | ΛNode::Α(_, _) => {}
+            // TODO: think about evaling the exprs
+            ΛNode::Χ(name, expr) => {
+                self.vardefs.insert(name, *expr);
+            }
+        };
+        tree
     }
 }
 
@@ -249,9 +241,9 @@ mod tests {
         ] {
             let mut split = case.split(" → ");
             let tree = &ΛCalculus::parse(split.next().unwrap())[0];
-            let βnf = ΛCalculus::parse(split.next().unwrap())[0];
+            let normal_form = ΛCalculus::parse(split.next().unwrap())[0];
             assert_eq!(tree, ΛCalculus::parse(&tree.to_string())[0]);
-            assert_eq!(tree.β_reduce(), βnf);
+            assert_eq!(tree.reduce(), normal_form);
         }
     }
 }
@@ -265,12 +257,13 @@ fn main() -> io::Result<()> {
 
     // TODO: print variable content when just the variable is given
 
+    // TODO: variables
+
     for raw_expr in exprs {
         let expr = raw_expr;
         println!("{:?}", expr);
         println!("expression: {}", expr.to_string());
-        println!("β-normal-form: {}", expr.β_reduce().to_string());
-        println!("η-normal-form: {}", expr.η_reduce().to_string());
+        println!("η-β-normal-form: {}", expr.reduce().to_string());
         // TODO: combine?
     }
 
