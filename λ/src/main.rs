cbb4613e9ca77596b7da1e81b6e73b63f37e3981
pixel <pixel@chrissx.de>
2022-08-12 14:35:36 +0200

    cli: add support for "running scripts"

--- λ/src/main.rs
+++ λ/src/main.rs
@@ -3,12 +3,14 @@ use clap::Parser;
 use permissions::*;
 use rustyline::{config::Builder, error::ReadlineError, EditMode, Editor};
 use shellexpand::tilde;
-use std::{fs::canonicalize, path::Path, path::PathBuf};
+use std::{fs::canonicalize, fs::read_to_string, path::Path, path::PathBuf};
 
 #[derive(Parser, Debug)]
 #[clap(version)]
 struct Args {
     // TODO: come up with some more
+    #[clap(value_parser)]
+    files: Vec<String>,
 
     // TODO: think about making this action = clap::ArgAction::Count and int
     #[clap(short, long)]
@@ -57,6 +59,7 @@ fn main() -> Result<(), ReadlineError> {
     let args = Args::parse();
     let mut ec = Result::<(), ReadlineError>::Ok(());
 
+    // TODO: dont even set up the readline if we're not using a repl
     let mut rl = Editor::<()>::with_config(
         Builder::new()
             .edit_mode(if args.vi {
@@ -88,35 +91,63 @@ fn main() -> Result<(), ReadlineError> {
         ΛCalculus::with_stdlib(args.verbose)
     };
 
-    loop {
-        match rl.readline("λ> ") {
-            Ok(line) => {
-                // TODO: think about not putting some things (maybe errors) in the history
-                // FIXME: weirdly doesnt seem to work...︎︎︎︎︎⸘
-                rl.add_history_entry(line.as_str());
-                // TODO: catch parser errors
-                match ΛCalculus::parse(&line, args.verbose) {
-                    Ok(x) => {
-                        for expr in x {
-                            if args.verbose {
-                                // TODO: think about if these are the right things to list (as above)
-                                println!("expression:     {:?}", expr);
-                                println!("as string:      {}", expr.to_string());
-                                println!("normal-form:    {}", calc.eval(expr).to_string());
-                            } else {
-                                println!("{}", calc.eval(expr).to_string());
+    if args.files.len() == 0 {
+        // read
+        // eval
+        // print
+        loop {
+            match rl.readline("λ> ") {
+                Ok(line) => {
+                    // TODO: think about not putting some things (maybe errors) in the history
+                    // FIXME: weirdly doesnt seem to work...︎︎︎︎︎⸘
+                    rl.add_history_entry(line.as_str());
+                    // TODO: catch parser errors
+                    match ΛCalculus::parse(&line, args.verbose) {
+                        Ok(x) => {
+                            for expr in x {
+                                if args.verbose {
+                                    // TODO: think about if these are the right things to list (as above)
+                                    println!("expression:     {:?}", expr);
+                                    println!("as string:      {}", expr.to_string());
+                                    println!("normal-form:    {}", calc.eval(expr).to_string());
+                                } else {
+                                    println!("{}", calc.eval(expr).to_string());
+                                }
                             }
                         }
+                        Err(err) => eprintln!("{}", err),
                     }
-                    Err(err) => println!("{}", err),
+                }
+                Err(ReadlineError::Interrupted) => {}
+                Err(ReadlineError::Eof) => break,
+                Err(err) => {
+                    ec = Err(err);
+                    break;
                 }
             }
-            Err(ReadlineError::Interrupted) => {}
-            Err(ReadlineError::Eof) => break,
-            Err(err) => {
-                ec = Err(err);
-                break;
-            }
+        }
+    } else {
+        for path in args.files {
+            match read_to_string(&path) {
+                Err(err) => panic!("couldn't read {}: {}", path, err),
+                Ok(rawcontents) => {
+                    match ΛCalculus::parse(&rawcontents, args.verbose) {
+                        Ok(x) => {
+                            for expr in x {
+                                // TODO: think about not printing anything if we implement something like an io monad
+                                if args.verbose {
+                                    println!("expression:     {:?}", expr);
+                                    println!("as string:      {}", expr.to_string());
+                                    println!("normal-form:    {}", calc.eval(expr).to_string());
+                                } else {
+                                    println!("{}", calc.eval(expr).to_string());
+                                }
+                            }
+                        }
+                        Err(err) => eprintln!("{}", err),
+                    }
+                }
+            };
         }
     }
 
